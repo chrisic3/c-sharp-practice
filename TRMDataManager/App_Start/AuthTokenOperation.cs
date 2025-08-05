e4a000d134269cfc3a3c62fd13eb31f43b4fa7fc
chrisic3 <chris_ic3@hotmail.com>
2025-08-04 22:34:58 -0500

    Configure Swagger
    1. Tweak default settings in SwaggerConfig
    2. Add the AuthTokenOperation class implementing IDocumentFilter
    3. Implement the Apply method (this adds a route for /token)
    4. Add the document filter to the SwaggerConfig
    5. Add the AuthorizationOperationFilter class Implementing IOperationFilter
    6. Implement the Apply method
    7. Add the operation filter to the SwaggerConfig

---
+++ TRMDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,55 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        // https://stackoverflow.com/questions/51117655/how-to-use-swagger-in-asp-net-webapi-2-0-with-token-based-authentication
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                // This may need to be a get command
+                // This matches the settings used in Postman for /token
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
